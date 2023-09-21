b8c2270d5beff502a69fc69ad74efeb7d4ffd56d
Hansjoerg <hansjoeg.wingeier@gmail.com>
2023-09-21 06:58:56 +0200

    finished notebooks

--- secfsdstools/e_presenter/presenting.py
+++ secfsdstools/e_presenter/presenting.py
@@ -13,7 +13,8 @@ class StandardStatementPresenter(Presenter[JoinedDataBag]):
     financial statements are presented.
     """
 
-    def __init__(self, flatten_index: bool = True, add_form_column: bool = False):
+    def __init__(self, flatten_index: bool = True, add_form_column: bool = False,
+                 invert_negating: bool = False):
         """
 
         Args:
@@ -23,10 +24,18 @@ class StandardStatementPresenter(Presenter[JoinedDataBag]):
                              appear as columns and not in the index of the dataframe.
             add_form_column: adds a "form" column to the resulting dataframe which contains
                              the type of the report ('10-K', '10-Q', ...)
+            invert_negating: In a report, values are sometimes shown as positive even when
+                             from a calculating perspective they are negative, and vice versa.
+                             Such values are marked with a "1" in the negating column.
+                             When this flag is true, the value of such tags where negating is set
+                             to 1 is inverted. A typical case is "paiddividends" where the value
+                             is shown as positive but from a calculating point of view it should
+                             be negative.
         """
 
         self.flatten_index = flatten_index
         self.add_form_column = add_form_column
+        self.invert_negating = invert_negating
 
     def present(self, databag: JoinedDataBag) -> pd.DataFrame:
         """
@@ -45,7 +54,12 @@ class StandardStatementPresenter(Presenter[JoinedDataBag]):
             pd.DataFrame: the dataframe with the final presentation
         """
 
-        num_pre_pivot_df = databag.pre_num_df.pivot_table(
+        pre_num_df = databag.pre_num_df
+        if self.invert_negating:
+            pre_num_df = pre_num_df.copy()
+            pre_num_df.loc[pre_num_df.negating == 1, 'value'] = -pre_num_df.value
+
+        num_pre_pivot_df = pre_num_df.pivot_table(
             index=['adsh', 'coreg', 'tag', 'version', 'stmt',
                    'report', 'line', 'uom', 'negating', 'inpth'],
             columns='ddate',

d80c87e96c265d1f8f7d076111672373d19dbc38
Hansjoerg <hansjoeg.wingeier@gmail.com>
2024-02-17 08:33:22 +0100

    preparations for is standardizer development

--- sandbox/analyze_is_structure.py
+++ sandbox/analyze_is_structure.py
@@ -1,11 +1,15 @@
 import time
+from typing import List
+
+import pandas as pd
 
 from secfsdstools.d_container.databagmodel import JoinedDataBag
 from secfsdstools.e_collector.companycollecting import CompanyReportCollector
+from secfsdstools.e_filter.joinedfiltering import FilterBase
 from secfsdstools.e_filter.rawfiltering import ReportPeriodRawFilter, MainCoregRawFilter, \
     OfficialTagsOnlyRawFilter, USDOnlyRawFilter
-from secfsdstools.f_standardize.standardizing import StandardizedBag
 from secfsdstools.f_standardize.is_standardize import IncomeStatementStandardizer
+from secfsdstools.f_standardize.standardizing import StandardizedBag
 
 
 def timing(f):
@@ -40,6 +44,10 @@ def load_smaller_sample_IS_set() -> JoinedDataBag:
     return JoinedDataBag.load("./saved_data/is_small_joined")
 
 
+def filter_tags(pre_num_df: pd.DataFrame, tag_like: str) -> List[str]:
+    return [x for x in pre_num_df.tag.unique().tolist() if "Revenue" in x]
+
+
 @timing
 def standardize(is_joined_bag: JoinedDataBag) -> StandardizedBag:
     is_standardizer = IncomeStatementStandardizer()
@@ -47,15 +55,47 @@ def standardize(is_joined_bag: JoinedDataBag) -> StandardizedBag:
     return is_standardizer.get_standardize_bag()
 
 
+class ISQrtrsFilter(FilterBase):
+    """
+    Filters the data, so that only datapoints for 4 qrtrs for 10-K,
+    and 1 qtrs for 10-Q are kept.
+    """
+
+    def filter(self, bag: JoinedDataBag) -> JoinedDataBag:
+        # Temporäres DataFrame für "form" hinzufügen
+        temp_pre_num_df = pd.merge(bag.pre_num_df, bag.sub_df[['adsh', 'form']], on='adsh', how='inner')
+
+        # Filterkriterien
+        criteria = (
+                ((temp_pre_num_df['form'] == '10-K') & (temp_pre_num_df['qtrs'] == 4)) |
+                ((temp_pre_num_df['form'] == '10-Q') & (temp_pre_num_df['qtrs'] == 1))
+        )
+
+        # Ergebnis DataFrame B filtern
+        pre_num_df = temp_pre_num_df[criteria]
+        del pre_num_df['form']
+        return JoinedDataBag.create(sub_df=bag.sub_df, pre_num_df=pre_num_df)
+
+
 if __name__ == '__main__':
     # create_smaller_sample_IS_set()
-    # is_joined_bag = load_joined_IS_set()
 
-    is_joined_bag = load_smaller_sample_IS_set()
+    is_joined_bag: JoinedDataBag = load_joined_IS_set()
+    # is_joined_bag = load_smaller_sample_IS_set()
+
+    # print(filter_tags(is_joined_bag.pre_num_df, tag_like="Revenue"))
     #
     # # check the loaded data
-    # print(is_joined_bag.sub_df.shape)
-    #
-    # standardized_bag = standardize(is_joined_bag)
-    #
-    # print(standardized_bag.result_df.shape)
+    print("sub_df", is_joined_bag.sub_df.shape)
+    print("pre_num_df", is_joined_bag.pre_num_df.shape)
+
+    is_joined_bag_filtered = is_joined_bag[ISQrtrsFilter()]
+    print("filtered pre_num_df", is_joined_bag_filtered.pre_num_df.shape)
+
+
+
+    standardized_bag = standardize(is_joined_bag_filtered)
+
+    print(standardized_bag.result_df.shape)
+
+    print("wait")

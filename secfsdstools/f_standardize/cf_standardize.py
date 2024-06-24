dddd22dcc77839397cac79dc16dd7dbd9a6cef8c
Hansjoerg <hansjoeg.wingeier@gmail.com>
2024-06-24 11:09:56 +0200

    fixe Numpy to < 2.0

--- secfsdstools/f_standardize/cf_standardize.py
+++ secfsdstools/f_standardize/cf_standardize.py
@@ -333,130 +333,36 @@ class CashFlowStandardizer(Standardizer):
 
     Final Tags
         NetCashProvidedByUsedInOperatingActivities
-        1.1. AdjustmentsToReconcileNetIncomeLossToCashProvidedByUsedInOperatingActivities (not used)
-        1.1.1. DepreciationDepletionAndAmortization             ok Hierarchy
-        1.1.2. DeferredIncomeTaxExpenseBenefit                  ok direct
-        1.1.3. ShareBasedCompensation                           ok direct
-        1.1.7. IncreaseDecreaseInAccountsPayable                ok direct
-        1.1.8. IncreaseDecreaseInAccruedLiabilities             ok direct
-        1.2. OtherAdjustmentsToCashProvidedByUsedInOperatingActivities <- not present
-        1.2.2. InterestPaidNet                                  ok direct
-        1.2.3. IncomeTaxesPaidNet                               ok direct
+          DepreciationDepletionAndAmortization                 ok Hierarchy
+          DeferredIncomeTaxExpenseBenefit                      ok direct
+          ShareBasedCompensation                               ok direct
+          IncreaseDecreaseInAccountsPayable                    ok direct
+          IncreaseDecreaseInAccruedLiabilities                 ok direct
+          InterestPaidNet                                      ok direct
+          IncomeTaxesPaidNet                                   ok direct
 
         NetCashProvidedByUsedInInvestingActivities
-        2.1. Investments in Property, Plant, and Equipment
-        2.1.1. PaymentsToAcquirePropertyPlantAndEquipment       ok direct
-        2.1.2. ProceedsFromSaleOfPropertyPlantAndEquipment      ok direct
-        2.2. Investments in Securities
-        2.2.1. PaymentsToAcquireInvestments                     ok direct
-        2.2.2. ProceedsFromSaleOfInvestments                    ok hierarchy
-        2.3. Business Acquisitions and Divestitures
-        2.3.1. PaymentsToAcquireBusinessesNetOfCashAcquired     ok direct
-        2.3.2. ProceedsFromDivestitureOfBusinessesNetOfCashDivested ok direct
-        2.4. OtherCashProvidedByUsedInInvestingActivities
-        2.4.1. Investments in Intangible Assets
-        2.4.1.1. PaymentsToAcquireIntangibleAssets              ok direct
-        2.4.1.2. ProceedsFromSaleOfIntangibleAssets             ok direct
+          PaymentsToAcquirePropertyPlantAndEquipment           ok direct
+          ProceedsFromSaleOfPropertyPlantAndEquipment          ok direct
+          PaymentsToAcquireInvestments                         ok direct
+          ProceedsFromSaleOfInvestments                        ok Hierarchy
+          PaymentsToAcquireBusinessesNetOfCashAcquired         ok direct
+          ProceedsFromDivestitureOfBusinessesNetOfCashDivested ok direct
+          PaymentsToAcquireIntangibleAssets                    ok direct
+          ProceedsFromSaleOfIntangibleAssets                   ok direct
 
         NetCashProvidedByUsedInFinancingActivities
-        3.1. Equity Transactions
-        3.1.1. ProceedsFromIssuanceOfCommonStock                ok direct
-        3.1.3. ProceedsFromStockOptionsExercised                ok direct
-        3.1.4. PaymentsForRepurchaseOfCommonStock               ok direct
-        3.2. Debt Transactions
-        3.2.1. ProceedsFromIssuanceOfDebt                       ok direct
-        3.2.2. RepaymentsOfDebt                                 ok direct
-        3.3. PaymentsOfDividends                                ok Hierarchy
+          ProceedsFromIssuanceOfCommonStock                    ok direct
+          ProceedsFromStockOptionsExercised                    ok direct
+          PaymentsForRepurchaseOfCommonStock                   ok direct
+          ProceedsFromIssuanceOfDebt                           ok direct
+          RepaymentsOfDebt                                     ok direct
+          PaymentsOfDividends                                  ok Hierarchy
 
 
         NetIncreaseDecreaseInCashAndCashEquivalents
         4.1. CashAndCashEquivalentsPeriodIncreaseDecrease
         4.2. EffectOfExchangeRateOnCashAndCashEquivalents
-
-
-    Details
-    1.    NetCashProvidedByUsedInOperatingActivities
-    1.1.        AdjustmentsToReconcileNetIncomeLossToCashProvidedByUsedInOperatingActivities
-    1.1.1.            DepreciationDepletionAndAmortization
-    1.1.2.            DeferredIncomeTaxExpenseBenefit
-    1.1.3.            ShareBasedCompensation
-    1.1.4.            IncreaseDecreaseInAccountsReceivable
-    1.1.5.            IncreaseDecreaseInInventories
-    1.1.6.            IncreaseDecreaseInPrepaidDeferredExpenseAndOtherAssets
-    1.1.7.            IncreaseDecreaseInAccountsPayable
-    1.1.8.            IncreaseDecreaseInAccruedLiabilities
-    1.1.9.            IncreaseDecreaseInOtherOperatingActivities
-    1.1.10.           OtherNoncashIncomeExpense
-    1.2.        OtherAdjustmentsToCashProvidedByUsedInOperatingActivities
-    1.2.1.            NetIncomeLoss
-    1.2.2.            InterestPaidNet
-    1.2.3.            IncomeTaxesPaidNet
-    1.2.4.            DividendsReceived
-    1.2.5.            InterestReceived
-    1.2.6.            IncreaseDecreaseInOperatingCapital
-    1.2.7.            PaymentsForAssetRetirementObligations
-    1.2.8.            PaymentsForProvisions
-    1.2.9.            PaymentsForRestructuring
-
-    2.   NetCashProvidedByUsedInInvestingActivities
-    2.1.       Investments in Property, Plant, and Equipment
-    2.1.1.           PaymentsToAcquirePropertyPlantAndEquipment
-    2.1.2.           ProceedsFromSaleOfPropertyPlantAndEquipment
-    2.2.      Investments in Securities
-    2.2.1.           PaymentsToAcquireInvestments
-    2.2.2.           ProceedsFromSaleOfInvestments
-    2.2.3.           PaymentsToAcquireHeldToMaturitySecurities
-    2.2.4.           ProceedsFromMaturitiesPrepaymentsAndCallsOfHeldToMaturitySecurities
-    2.2.5.           PaymentsToAcquireAvailableForSaleSecurities
-    2.2.6.           ProceedsFromSaleOfAvailableForSaleSecurities
-    2.2.7.           PaymentsToAcquireTradingSecurities
-    2.2.8.           ProceedsFromSaleOfTradingSecurities
-    2.3.       Business Acquisitions and Divestitures
-    2.3.1.           PaymentsToAcquireBusinessesNetOfCashAcquired
-    2.3.2.           ProceedsFromDivestitureOfBusinessesNetOfCashDivested
-    2.3.3.           PaymentsMadeInConnectionWithBusinessAcquisitions
-    2.3.4.           ProceedsFromBusinessAcquisitionsNetOfCashAcquired
-    2.4.       OtherCashProvidedByUsedInInvestingActivities
-    2.4.1.           Investments in Intangible Assets
-    2.4.1.1.              PaymentsToAcquireIntangibleAssets
-    2.4.1.2.              ProceedsFromSaleOfIntangibleAssets
-    2.4.2.           Loans and Advances
-    2.4.2.1.               ProceedsFromRepaymentsOfLoansAndAdvancesToOtherEntities
-    2.4.2.2.               PaymentsForLoansAndAdvancesToOtherEntities
-    2.4.3.           Joint Ventures
-    2.4.3.1.               PaymentsToAcquireJointVenture
-
-    3.    NetCashProvidedByUsedInFinancingActivities
-    3.1.        Equity Transactions
-    3.1.1.            ProceedsFromIssuanceOfCommonStock
-    3.1.2.            ProceedsFromIssuanceOfPreferredStock
-    3.1.3.            ProceedsFromStockOptionsExercised
-    3.1.4.            PaymentsForRepurchaseOfCommonStock
-    3.1.5.            PaymentsForRepurchaseOfPreferredStock
-    3.1.6.            ProceedsFromIssuanceOfOtherEquityInstruments
-    3.1.7.            PaymentsForRepurchaseOfOtherEquityInstruments
-    3.1.8.            ProceedsFromSaleOfTreasuryStock
-    3.1.9.            PaymentsToAcquireTreasuryStock
-    3.2.      Debt Transactions
-    3.2.1.          ProceedsFromIssuanceOfDebt
-    3.2.2.          RepaymentsOfDebt
-    3.2.3.          ProceedsFromBorrowings
-    3.2.4.          RepaymentsOfBorrowings
-    3.3.       Dividends
-    3.3.1.           DividendsPaid
-    3.3.1.1.               DividendsPaidToNoncontrollingInterest
-    3.3.1.2.               DividendsPaidToControllingInterest
-    3.4.       Leases
-    3.4.1.           PaymentsOfFinanceLeaseObligations
-    3.4.2.           PaymentsOfOperatingLeaseLiabilities
-    3.5.        Grants and Other Financing Sources
-    3.5.1.            ProceedsFromGovernmentGrants
-
-    4.    NetIncreaseDecreaseInCashAndCashEquivalents
-    4.1.        CashAndCashEquivalentsPeriodIncreaseDecrease
-    4.2.        EffectOfExchangeRateOnCashAndCashEquivalents
-    4.3.        CashAndCashEquivalentsAtCarryingValue
-    4.4.        CashAndCashEquivalentsBeginningOfPeriod
 
     </pre>
 

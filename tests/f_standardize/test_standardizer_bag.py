ab67be5b2e62a19e808dab8bf939a7c3e2871400
Hansjoerg <hansjoeg.wingeier@gmail.com>
2024-08-17 07:20:33 +0200

    Test for concatenation of Standardized Bag

--- tests/f_standardize/test_standardizer_bag.py
+++ tests/f_standardize/test_standardizer_bag.py
@@ -1,27 +1,30 @@
-import pytest
 import os
+
+import pytest
 
 from secfsdstools.f_standardize.standardizing import StandardizedBag
 
 CURRENT_DIR, _ = os.path.split(__file__)
 
+
 @pytest.fixture
 def sample_bag1() -> StandardizedBag:
-    yield StandardizedBag.load(CURRENT_DIR + "/../_testdata/is_standardized")
+    yield StandardizedBag.load(f"{CURRENT_DIR}/../_testdata/is_standardized")
+
 
 @pytest.fixture
 def sample_bag2() -> StandardizedBag:
-    yield StandardizedBag.load(CURRENT_DIR + "/../_testdata/is_standardized_2")
+    yield StandardizedBag.load(f"{CURRENT_DIR}/../_testdata/is_standardized_2")
 
 
 def test_concatenate(sample_bag1, sample_bag2):
-    print(sample_bag2.result_df.shape)
-    print(sample_bag1.result_df.shape)
     result = StandardizedBag.concat([sample_bag2, sample_bag1])
 
-    # todo: checks
+    # do some basic checks
+    assert result.result_df.shape == (327, 36)
+    assert result.applied_prepivot_rules_log_df.shape == (3780, 9)
+    assert result.applied_rules_log_df.shape == (327, 168)
+    assert result.applied_rules_sum_s.shape == (164, )
+    assert result.stats_df.shape == (12, 14)
 
     print(result.result_df.shape)
-
-
-

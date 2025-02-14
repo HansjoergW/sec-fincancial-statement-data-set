30d1e652465a4055e9ca58614182c1006a51fba5
Hansjoerg <hansjoeg.wingeier@gmail.com>
2025-02-14 16:36:50 +0100

    added concat_bags_file_based in pipeline_utils

--- secfsdstools/g_pipelines/pipeline_utils.py
+++ secfsdstools/g_pipelines/pipeline_utils.py
@@ -4,6 +4,10 @@ Util functions used in pipeline tasks
 from pathlib import Path
 from typing import List
 
+import pandas as pd
+
+from secfsdstools.a_utils.constants import SUB_TXT, NUM_TXT, PRE_TXT, PRE_NUM_TXT
+from secfsdstools.a_utils.fileutils import concat_parquet_files
 from secfsdstools.d_container.databagmodel import RawDataBag, JoinedDataBag, is_rawbag_path, \
     is_joinedbag_path
 
@@ -17,10 +21,8 @@ def concat_bags(paths_to_concat: List[Path], target_path: Path):
     Of course, all paths in the paths_to_concat must be of the same type
 
     Args:
-        paths_to_concat (List[Path]) :
-        target_dir:
-
-    Returns:
+        paths_to_concat (List[Path]) : List with paths to read the datafrome
+        target_path (Path) : path to write the concatenated data to
 
     """
     if len(paths_to_concat) == 0:
@@ -37,5 +39,70 @@ def concat_bags(paths_to_concat: List[Path], target_path: Path):
 
         all_bag: JoinedDataBag = JoinedDataBag.concat(all_bags, drop_duplicates_sub_df=True)
         all_bag.save(target_path=str(target_path))
+    else:
+        raise ValueError("bag_type must be either raw or joined")
+
+
+def _concat_bags_file_based_internal(paths_to_concat: List[Path],
+                                     target_path: Path,
+                                     file_list: List[str],
+                                     drop_duplicates_sub_df: bool = False):
+    target_path.mkdir(parents=True, exist_ok=True)
+    if not drop_duplicates_sub_df:
+        file_list.append(SUB_TXT)
+
+    for file_name in file_list:
+        target_path_file = str(target_path / f'{file_name}.parquet')
+        paths_to_concat_file = [str(p / f'{file_name}.parquet') for p in paths_to_concat]
+        concat_parquet_files(paths_to_concat_file, target_path_file)
+
+    # if we have to drop the duplicates, we need to read the data for the sub into memory
+    if drop_duplicates_sub_df:
+        sub_dfs: List[pd.DataFrame] = []
+        for path_to_concat in paths_to_concat:
+            sub_dfs.append(pd.read_parquet(path_to_concat / f'{SUB_TXT}.parquet'))
+
+        sub_df = pd.concat(sub_dfs, ignore_index=True)
+        sub_df.drop_duplicates(inplace=True)
+        sub_df.to_parquet(target_path / f'{SUB_TXT}.parquet')
+
+
+def concat_bags_file_based(paths_to_concat: List[Path],
+                           target_path: Path,
+                           drop_duplicates_sub_df: bool = False):
+    """
+    Concatenates all the Bags in paths_to_concatenate by using the provided bag_type
+    into the target_dir directory.
+
+    The logic checks for the type of the bag (Raw or Joined) and handles them accordingly.
+    Of course, all paths in the paths_to_concat must be of the same type
+
+    Args:
+        paths_to_concat (List[Path]) : List with paths to read the datafrome
+        target_path (Path) : path to write the concatenated data to
+        drop_duplicates_sub_df (bool, False) : if the final sub_df should be check for duplicates
+
+    Returns:
+    """
+    if len(paths_to_concat) == 0:
+        # nothing to do
+        return
+
+    if is_rawbag_path(paths_to_concat[0]):
+        file_list = [NUM_TXT, PRE_TXT]
+        _concat_bags_file_based_internal(
+            paths_to_concat=paths_to_concat,
+            target_path=target_path,
+            file_list=file_list,
+            drop_duplicates_sub_df=drop_duplicates_sub_df
+        )
+    elif is_joinedbag_path(paths_to_concat[0]):
+        file_list = [PRE_NUM_TXT]
+        _concat_bags_file_based_internal(
+            paths_to_concat=paths_to_concat,
+            target_path=target_path,
+            file_list=file_list,
+            drop_duplicates_sub_df=drop_duplicates_sub_df
+        )
     else:
         raise ValueError("bag_type must be either raw or joined")
--- tests/g_pipelines/test_pipeline_utils.py
+++ tests/g_pipelines/test_pipeline_utils.py
@@ -4,7 +4,7 @@ from typing import List
 
 from secfsdstools.d_container.databagmodel import JoinedDataBag
 
-from secfsdstools.g_pipelines.pipeline_utils import concat_bags
+from secfsdstools.g_pipelines.pipeline_utils import concat_bags, concat_bags_file_based
 
 CURRENT_DIR, _ = os.path.split(__file__)
 TESTDATA_PATH = Path(CURRENT_DIR) / ".." / "_testdata"
@@ -21,4 +21,49 @@ def test_concat_bags(tmp_path):
     bag = JoinedDataBag.load(target_path=str(tmp_path))
     # simply check if data were successful stored and loaded
     print(bag.sub_df.shape)
-    assert len(bag.sub_df) > 0
+    assert bag.sub_df.shape == (1017, 36)
+    assert bag.pre_num_df.shape == (398781, 17)
+
+
+def test_concat_bags_by_file(tmp_path):
+    parts: List[Path] = [
+        TESTDATA_PATH / "joined" / "2010q1.zip",
+        TESTDATA_PATH / "joined" / "2010q2.zip",
+        ]
+
+    concat_bags_file_based(paths_to_concat=parts, target_path=tmp_path)
+
+    bag = JoinedDataBag.load(target_path=str(tmp_path))
+    # simply check if data were successful stored and loaded
+    print(bag.sub_df.shape)
+    assert bag.sub_df.shape == (1017, 36)
+    assert bag.pre_num_df.shape == (398781, 17)
+
+def test_concat_bags_by_file_drop_duplicates(tmp_path):
+    parts: List[Path] = [
+        TESTDATA_PATH / "joined" / "2010q1.zip",
+        TESTDATA_PATH / "joined" / "2010q2.zip",
+        ]
+
+    concat_bags_file_based(paths_to_concat=parts, target_path=tmp_path, drop_duplicates_sub_df=True)
+
+    bag = JoinedDataBag.load(target_path=str(tmp_path))
+    # simply check if data were successful stored and loaded
+    print(bag.sub_df.shape)
+    assert bag.sub_df.shape == (1017, 36)
+    assert bag.pre_num_df.shape == (398781, 17)
+
+def test_concat_bags_by_file_drop_duplicates_2(tmp_path):
+    """ read the same file twice, so the sub_df should be different"""
+    parts: List[Path] = [
+        TESTDATA_PATH / "joined" / "2010q1.zip",
+        TESTDATA_PATH / "joined" / "2010q1.zip",
+        ]
+
+    concat_bags_file_based(paths_to_concat=parts, target_path=tmp_path, drop_duplicates_sub_df=True)
+
+    bag = JoinedDataBag.load(target_path=str(tmp_path))
+    # simply check if data were successful stored and loaded
+    print(bag.sub_df.shape)
+    assert bag.sub_df.shape == (495, 36)
+    assert bag.pre_num_df.shape == (475432, 17)
